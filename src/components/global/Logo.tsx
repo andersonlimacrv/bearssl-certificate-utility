fb622aaa11f5a62b2d85976d5214239944324fc1
Anderson_Carvalho <andersonlimacrv@gmail.com>
2024-05-11 05:14:06 -0300

    deploy: fix logo image

--- src/components/global/Logo.tsx
+++ src/components/global/Logo.tsx
@@ -2,9 +2,9 @@ const Logo = () => {
 	return (
 		<div className="flex justify-center border-[1px] border-muted-foreground/20 rounded-full">
 			<img
-				src="/assets/AC.svg"
-				alt=""
-				className="h-14 drop-shadow-1xl"
+				src="../../assets/LOGO.png"
+				alt="aa"
+				className="h-14 w-full drop-shadow-1xl"
 			/>
 		</div>
 	);
