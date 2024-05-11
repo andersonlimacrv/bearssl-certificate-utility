0a12115d974b9860c0cbde3b6c3f099dac8e0e4b
Anderson_Carvalho <andersonlimacrv@gmail.com>
2024-05-11 04:24:03 -0300

    fix: deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import Navbar from '@/components/global/navbar';
 import Layout from '@/components/global/Layout';
 import Hero from '@/components/Hero';
 import Background from '@/components/global/Background';
+
 function App() {
 	return (
 		<>
